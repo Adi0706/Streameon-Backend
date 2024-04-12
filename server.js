e184415380296eff26176adede374833b0374c90
Aditya Bhattacharjee <adityab76@gmail.com>
2024-04-12 14:53:02 +0530

    signup route and api done

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
-const express = require('express') ; 
-const cors = require('cors')
-const dotenv = require('dotenv') ; 
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const SignupModel = require("./Models/SignupModel.js");
+const bcrypt = require('bcrypt');
 
+dotenv.config();
 
-dotenv.config() ; 
-const app = express() ; 
-const PORT_NUMBER = process.env.PORT
+const app = express();
+const PORT_NUMBER = process.env.PORT;
+const MONGOURL = process.env.MONGO_ATLAS_CONNECTION_URL;
 
-app.use(cors()) ; 
-app.use(express.json()) ; 
+try {
+    mongoose.connect(MONGOURL);
+    console.log("MongoDb connected!");
+} catch (err) {
+    console.log("Connection error: " + err);
+}
 
-app.get('/',(req,res)=>{
-    res.send("hi i am server")
-})
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
+app.post('/api/Signup', async (req, res) => {
+    const { name, email, password } = req.body;
 
-app.listen(PORT_NUMBER,()=>{
-    console.log(`server is running on Port Number  ${PORT_NUMBER}`) ; 
-})
+    try {
+        // Check if user with the same email already exists
+        const existingUser = await SignupModel.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+
+        // Hashing the password before saving it to the database
+        const hashedPassword = await bcrypt.hash(password, 10); // Generated a salted hash with a complexity factor of 10
+
+        // Created a new user with the hashed password
+        const newSignup = await SignupModel.create({ name, email, password: hashedPassword });
+        console.log("New User Created", newSignup);
+
+        // Sending success response to frontend 
+        return res.status(201).json({ message: "Success" });
+    } catch (error) {
+        console.log("Error Signing Up", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.listen(PORT_NUMBER, () => {
+    console.log(`Server is running on Port Number ${PORT_NUMBER}`);
+});
